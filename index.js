d5d8aa711bcc7a5e8bc42efdb9d37a34ac3ef0f0
Amir Parvinazar <amirparvinazar23@gmail.com>
2024-01-15 14:35:21 +0330

    developing

--- index.js
+++ index.js
@@ -6,26 +6,18 @@ const bot = new TelegramBot('6812587488:AAFQAmQpyE6oI5_RT0KFZKqH0KIAG3CgaYw', { 
 // Replace 'YOUR_CHANNEL_ID' with your actual channel ID
 const channel = '-1001573630378';
 
-// Set the start command message
-bot.setMyCommands([
-  { command: 'start', description: 'Start the bot' },
-]);
-
 // Event listener for incoming messages
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   const messageText = msg.text;
 
-  // Handle the /start command separately
-  if (messageText.startsWith('/start')) {
-    // Send a specific message when /start command is used
-    bot.sendMessage(chatId, 'سلام! بهم پیام بفرست تا من با افزودن یه متن به کانالت بفرستم!');
-  } else {
-    // Append your specific text to the received message
-    const appendedMessage = messageText + '\n🌐 @testusername\n 🌐 @testusername2';
-
-    // Send the modified message to the channel
-    bot.sendMessage(channel, appendedMessage);
-    bot.sendMessage(msg.chat.id, "پیام ارسال شد!")
-  }
+    if (messageText === "/start") {
+        bot.sendMessage(chatId, "سلام! پیامت رو بفرست تا به کانالت هدایت کنم. یادت نره من رو در کانالت ادمین کنی.")
+    } else {
+        // Append your specific text to the received message
+        const appendedMessage = messageText + '\nراه های ارتباطی: \n🌐 @testusername\n🌐 @testusername2'; 
+        // Send the modified message to the channel
+        bot.sendMessage(channel, appendedMessage);
+        bot.sendMessage(msg.chat.id, "پیام ارسال شد!")
+    }
 });
